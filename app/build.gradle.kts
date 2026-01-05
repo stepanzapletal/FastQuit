e3edb57f701033da5ab7d80b8d78479c7ce97888
Štěpán Zapletal <stepula.zapletal@gmail.com>
2026-01-05 20:36:28 +0100

    1.2 source

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.stepan.fastquit"
         minSdk = 32
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         javaCompileOptions {
